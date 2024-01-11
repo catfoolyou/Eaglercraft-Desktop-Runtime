4df9cbf2e9d3dedcfa3f564a8d8b7c7c622535d8
CatFoolYou <95507639+catfoolyou@users.noreply.github.com>
2024-01-11 12:55:17 -0500

    Add files via upload

--- launch.bat
+++ launch.bat
@@ -1,5 +1,8 @@
 @echo off
 SET dir=%cd%/natives
-title fuck off
+
+call ./java/bukkit_command/run.bat"
+call ./java/bungee_command/run.bat"
 
 java -Xmx1G -Xms1G "-Djava.library.path=%dir%" -cp "eaglercraft.jar;java-websocket.jar;lwjgl.jar;lwjgl_util.jar" net.lax1dude.eaglercraft.MinecraftMain
+
