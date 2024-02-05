2a0b3fc9a743d2065acf13710903077b1d34cb0a
CatFoolYou <95507639+catfoolyou@users.noreply.github.com>
2024-02-05 08:40:52 -0500

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,8 +1,5 @@
 @echo off
 SET dir=%cd%/natives
 
-call ./java/bukkit_command/run.bat"
-call ./java/bungee_command/run.bat"
-
 java -Xmx1G -Xms1G "-Djava.library.path=%dir%" -cp "eaglercraft.jar;java-websocket.jar;lwjgl.jar;lwjgl_util.jar" net.lax1dude.eaglercraft.MinecraftMain
 
